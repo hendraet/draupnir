53215b9add7f93660188c32ccd8a80337fc46525
hendraet <hendrik.raetz@niederjahna.de>
2016-12-21 12:44:07 +0100

    added functionality to send pictures on it's own via chatlist

--- draupnir.py
+++ draupnir.py
@@ -16,19 +16,24 @@ class Draupnir:
     GIFV_PATTERN = re.compile(".*(\.gifv)")
     JPG_PATTERN = re.compile(".*(\.jpg)")
 
+    #-----------------------------Init----------------------------------------
     def __init__(self):
         self.read_config()
 
         self.reddit = praw.Reddit(user_agent=self.USER_AGENT, client_id=self.CLIENT_ID, client_secret=self.CLIENT_SECRET)
         self.bot = telepot.Bot(self.TOKEN)
 
-    def start(self):
-        try:
-            self.bot.message_loop(self.handle)
-            while 1:
-                time.sleep(10)
-        except KeyboardInterrupt:
-            sys.exit()
+    def start(self, arg_list):
+        print(arg_list)
+        if len(arg_list) > 1 and arg_list[1] == "daily":
+            self.send_special()
+        else:
+            try:
+                self.bot.message_loop(self.handle)
+                while 1:
+                    time.sleep(10)
+            except KeyboardInterrupt:
+                sys.exit()
 
     def read_config(self):
         config = open("config.ini", "r")
@@ -49,6 +54,7 @@ class Draupnir:
             print("Malformatted config file")
             sys.exit()
 
+    #-----------------------------General--------------------------------------
     def is_subreddit(self, subreddit_string):
         subreddits = []
         try:
@@ -62,14 +68,12 @@ class Draupnir:
             return False
 
     def parse_url(self, url):
-        print(url)
         if self.IMGUR_PATTERN.match(url) == None and self.IREDDIT_PATTERN.match(url) == None and self.REDDIT_UPLOADS_PATTERN.match(url) == None:
             return None
         elif self.GIF_PATTERN.match(url) != None:
             return (url, ".gif")
         elif self.GIFV_PATTERN.match(url) != None:
             new_url = re.sub("gifv", "gif", url)
-            print("(" + new_url + ")")
             return (new_url, ".gif")
         elif self.JPG_PATTERN.match(url) != None:
             return (url, ".jpg")
@@ -87,19 +91,17 @@ class Draupnir:
             if len(self.image_list) >= 3:
                 break;
 
+        print(len(self.image_list), self.image_list[0])
+
         if self.image_list:
-            #self.image_list = urllib.request.urlopen(image_list[0][0]), image_list[0][1]
             return True
         else:
             print("No images available")
             return False
 
-    def handle(self, msg):
-        content_type, chat_type, chat_id = telepot.glance(msg)
-        caption = msg["text"].strip("/")
-
-        if self.is_subreddit(caption) != False:
-            got_images = self.generate_images_for_subreddit(caption)
+    def send_image_for_subreddit(self, subreddit, chat_id):
+        if self.is_subreddit(subreddit) != False:
+            got_images = self.generate_images_for_subreddit(subreddit)
 
             if got_images == True:
                 filetype = self.image_list[0][1]
@@ -108,21 +110,40 @@ class Draupnir:
                 print("Done opening")
 
                 if filetype == ".gif":
-                    self.bot.sendDocument(chat_id, (caption + filetype, image))
+                    self.bot.sendDocument(chat_id, (subreddit + filetype, image))
                 elif filetype == ".jpg":
-                    self.bot.sendPhoto(chat_id, (caption + filetype, image))
+                    self.bot.sendPhoto(chat_id, (subreddit + filetype, image))
                 else:
                     print("Don't know how to send this")
                 print("Done sending")
-            
+            else:
+                print("Couldn't load any images")
+
         else:
             print("No subreddits with this name")
+
+    #------------------------------Daily/Special---------------------------------
+    def send_special(self):
+        chat_list = open("chatlist.txt", "r")
+        for line in chat_list.readlines():
+            line = line.rstrip()
+            chat_id, subreddit = line.split(":")
+            self.send_image_for_subreddit(subreddit, chat_id)
+
+    #------------------------------Normal Handle---------------------------------
+    def handle(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        print(chat_id)
+        caption = msg["text"].strip("/")
+
+        self.send_image_for_subreddit(caption, chat_id)
+
 #------------------------------------------------------------------
 
 #------------------------------Main--------------------------------
-def main():
+def main(arg_list):
     draupnir = Draupnir()
-    draupnir.start()
+    draupnir.start(arg_list)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
